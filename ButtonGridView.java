7c01fd6c61992fa956b7c1557bdff5e501e9af31
Nathan Morgenstern <ncmorgenstern@gmail.com>
2017-11-15 10:40:39 -0500

    Fixing button logic to speed things up slightly

--- ButtonGridView.java
+++ ButtonGridView.java
@@ -26,11 +26,6 @@ public class ButtonGridView extends JFrame{
           Color lightBrown = new Color(166, 123, 91);
           Color lightBlue  = new Color(173, 216, 230);
 
-          int start_x = s_e_pair.get(0).get_x_coordinate();
-          int start_y = s_e_pair.get(0).get_y_coordinate();
-
-          int goal_x  = s_e_pair.get(1).get_x_coordinate();
-          int goal_y  = s_e_pair.get(1).get_y_coordinate();
 
           //TODO: Add start and end goal to the map
           for(int x = 0; x < ROWS; x++)
@@ -39,38 +34,42 @@ public class ButtonGridView extends JFrame{
             {
               temp = new JButton(); //creates new JLabel
 
-              if(x == goal_x && y == goal_y)
-                temp.setBackground(Color.red);
 
-              else if(x == start_x && y == start_y)
-                temp.setBackground(Color.red);
+              if(char_grid[x][y] == '2')
+                temp.setBackground(lightBrown);
 
-              else
-              {
+              if(char_grid[x][y] == '0')
+                temp.setBackground(Color.black);
 
-                if(char_grid[x][y] == '2')
-                  temp.setBackground(lightBrown);
+              if(char_grid[x][y] == 'a' || char_grid[x][y] == 'b' )
+                temp.setBackground(lightBlue);
 
-                if(char_grid[x][y] == '0')
-                  temp.setBackground(Color.black);
+              temp.setOpaque(true);
+              temp.setBorder(border);
+              grid[x][y] = temp;
+              pane.add(grid[x][y]); //adds button to grid
 
-                if(char_grid[x][y] == 'a' || char_grid[x][y] == 'b' )
-                  temp.setBackground(lightBlue);
+            }
 
-              }
-
-            temp.setOpaque(true);
-            temp.setBorder(border);
-            grid[x][y] = temp;
-            pane.add(grid[x][y]); //adds button to grid
           }
+
+          int start_x = s_e_pair.get(0).get_x_coordinate();
+          int start_y = s_e_pair.get(0).get_y_coordinate();
+
+          int goal_x  = s_e_pair.get(1).get_x_coordinate();
+          int goal_y  = s_e_pair.get(1).get_y_coordinate();
+
+          grid[goal_x][goal_y].setBackground(Color.red);
+
+          grid[start_x][start_y].setBackground(Color.red);
+
 
           pack();
           setExtendedState(JFrame.MAXIMIZED_BOTH);
           setLocationRelativeTo(null);
           getContentPane().add(pane);
-        }
       }
+
 
         void addCellListener(ActionListener cellListener){
           for (int i = 0; i < ROWS; i++) {
